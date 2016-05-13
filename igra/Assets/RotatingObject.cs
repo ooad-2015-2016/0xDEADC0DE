c4a69e880a9424ca4de3ec6b24a5d866e4c7c8a8
Benjamin Ramic <bramic2@etf.unsa.ba>
2016-05-13 17:57:00 +0200

    Dodani bonusi i skripta za rotaciju.

---
+++ igra/Assets/RotatingObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up, 100.0f * Time.deltaTime);
+	}
+}
