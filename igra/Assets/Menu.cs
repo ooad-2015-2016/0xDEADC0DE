430d951d6ebea5278fbc11e9cadf0b1a8713e736
Muhamed Paric <mparic1@etf.unsa.ba>
2016-05-23 01:24:16 +0200

    Dodan rudimentaran (citaj: ruzan) pocetni meni

---
+++ igra/Assets/Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Button startGame;
+    public Button quitGame;
+
+    // Use this for initialization
+    void Start()
+    {
+        startGame.GetComponent<Button>();
+        quitGame.GetComponent<Button>();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
