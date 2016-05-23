7fdb5cfb84877d3637c9c78f57eaa12895a8fe4f
Muhamed Paric <mparic1@etf.unsa.ba>
2016-05-23 02:06:37 +0200

    Napravljen i (jos ruzniji) pause meni

---
+++ igra/Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Button btnResume;
+    public Button btnExit;
+
+    public bool isPaused;
+    public GameObject pauseMenuCanvas;
+
+	// Use this for initialization
+	public void Start ()
+    {
+        //btnResume.enabled = false;
+        //btnExit.enabled = false;
+	}
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+
+        //btnResume.enabled = false;
+        //btnExit.enabled = false;
+
+        //Time.timeScale = 1;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+	
+	// Update is called once per frame
+	public void Update ()
+    {
+	    if (isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+	}
+}
